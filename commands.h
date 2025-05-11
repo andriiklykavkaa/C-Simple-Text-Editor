b94a5458bb3f5f70d4619067830cd9f4912d5b87
andriiklykavkaa <andriiklykavkaa@gmail.com>
2025-05-11 19:25:02 +0300

    Basic configuration completed

---
+++ commands.h
@@ -0,0 +1,18 @@
+//
+// Created by Andrii Klykavka on 11.05.2025.
+//
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+void appendText();
+void addNewLine();
+void showInstructions();
+void saveFile();
+void loadFile();
+void printText();
+void insertByLineAndIndex();
+void searchText();
+void cleanConsole();
+
+#endif //COMMAND_H
--- main.c
+++ main.c
@@ -1,6 +1,55 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+#include "commands.h"
+#include "file_interaction.h"
+
+
+void handleCommand()
+{
+    int command;
+    printf("Choose the command: ");
+    scanf("%d", &command);
+
+    switch(command)
+    {
+        case 0:
+            showInstructions();
+            break;
+        case 1:
+            appendText();
+            break;
+        case 2:
+            addNewLine();
+            break;
+        case 3:
+            saveFile();
+            break;
+        case 4:
+            loadFile();
+            break;
+        case 5:
+            printText();
+            break;
+        case 6:
+            insertByLineAndIndex();
+            break;
+        case 7:
+            searchText();
+            break;
+    }
+}
+
+
+void cleanConsole() {
+
+}
+
+int main()
+{
+    printf("Welcome to the text editor!\n To see the list of commands, call command '0\n");
+    while(true)
+    {
+        handleCommand();
+    }
 }
