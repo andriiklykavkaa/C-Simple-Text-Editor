ce75ba6b40485f6ffda38967ec2d717c5f142569
andriiklykavkaa <andriiklykavkaa@gmail.com>
2025-05-17 15:58:38 +0300

    Terminal clear and code style enhencement

--- file_interaction.c
+++ file_interaction.c
@@ -5,26 +5,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void readFile(char path[], char buffer[255][255])
-{
+void readFile(char path[], char buffer[255][255]) {
     char line[255];
     int i = 0;
 
-    FILE * file;
+    FILE *file;
     file = fopen(path, "r");
 
-    if (file == NULL)
-    {
+    if (file == NULL) {
         printf("Error opening file at path '%s'", path);
-    }
-    else
-    {
+    } else {
         for (int j = 0; j < 255; j++) {
             buffer[j][0] = '\0';
         }
 
-        while (fgets(line, sizeof(line), file))
-        {
+        while (fgets(line, sizeof(line), file)) {
             strcpy(buffer[i], line);
             i++;
         }
@@ -34,23 +29,19 @@ void readFile(char path[], char buffer[255][255])
     }
 }
 
-void writeFile(char path[], char buffer[255][255])
-{
-    FILE* file;
+void writeFile(char path[], char buffer[255][255]) {
+    FILE *file;
     file = fopen(path, "w");
-    if (file != NULL)
-    {
+    if (file != NULL) {
         int i = 0;
-        while(buffer[i][0] != '\0')
-        {
+        while (buffer[i][0] != '\0') {
             fputs(buffer[i], file);
             fputc('\n', file);
             i++;
         }
 
         fclose(file);
-    } else
-    {
+    } else {
         printf("Error saving data to file!");
     }
 }
